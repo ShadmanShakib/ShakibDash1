f04b3a920239d95f7131eec281e23eb7dab8c876
Shadman Shakib <sm.shakib007@gmail.com>
2021-09-22 10:14:05 +0600

    redux setup

---
+++ src/services/PostApi.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+interface Post {
+  id: string;
+  body: string;
+  title: string;
+}
+
+// Define a service using a base URL and expected endpoints
+export const postApi = createApi({
+  reducerPath: "pokemonApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://jsonplaceholder.typicode.com/",
+  }),
+  endpoints: (builder) => ({
+    getPosts: builder.query<Post[], string>({
+      query: () => "posts",
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetPostsQuery } = postApi;
---
+++ src/store/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { postApi } from "../services/PostApi";
+export const store = configureStore({
+  reducer: {
+    [postApi.reducerPath]: postApi.reducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
