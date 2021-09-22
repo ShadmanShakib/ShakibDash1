d28387ad9fcd0da52f8879468fe9808c1fa8ee0e
Shadman Shakib <sm.shakib007@gmail.com>
2021-09-22 10:22:54 +0600

    Chakra added

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { store } from "./store/store";
-
+import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
