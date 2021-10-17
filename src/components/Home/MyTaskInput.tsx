7e0a2f5a1282828d75e696548074af2133b63cc1
Shadman Shakib <sm.shakib007@gmail.com>
2021-10-17 18:14:01 +0600

    ProjectWidget

--- src/components/Home/MyTaskInput.tsx
+++ src/components/Home/MyTaskInput.tsx
@@ -16,19 +16,14 @@ export default function MyTaskInput() {
         width="16px"
         focusable="false"
         viewBox="0 0 32 32"
-        style={{ alignSelf: "center", strokeWidth: "2px" }}
+        style={{ alignSelf: "center", strokeWidth: "2px", marginRight: "8px" }}
         fontSize="20px"
         display="block"
-        fill="#6d6e6f"
+        stroke="#6d6e6f"
+        fill="white"
       >
-        <path
-          fill="white"
-          d="M31,16c0,8.3-6.7,15-15,15S1,24.3,1,16S7.7,1,16,1S31,7.7,31,16z"
-        ></path>
-        <path
-          fill="#afabac"
-          d="M13.4,22.1c-0.3,0-0.5-0.1-0.7-0.3l-3.9-3.9c-0.4-0.4-0.4-1,0-1.4s1-0.4,1.4,0l3.1,3.1l8.1-8.1c0.4-0.4,1-0.4,1.4,0   s0.4,1,0,1.4l-8.9,8.9C13.9,22,13.7,22.1,13.4,22.1z"
-        ></path>
+        <path d="M31,16c0,8.3-6.7,15-15,15S1,24.3,1,16S7.7,1,16,1S31,7.7,31,16z"></path>
+        <path d="M13.4,22.1c-0.3,0-0.5-0.1-0.7-0.3l-3.9-3.9c-0.4-0.4-0.4-1,0-1.4s1-0.4,1.4,0l3.1,3.1l8.1-8.1c0.4-0.4,1-0.4,1.4,0   s0.4,1,0,1.4l-8.9,8.9C13.9,22,13.7,22.1,13.4,22.1z"></path>
       </svg>
       <Box display="inline-flex" w="100%" position="relative" mr="8px">
         <span className="FunTaskCreator-shadowSpan"></span>
---
+++ src/components/Home/ProjectWidget.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProjectWidget() {
+  return <div></div>;
+}
+
+export default ProjectWidget;
