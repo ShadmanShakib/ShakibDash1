21188b8c947fc885cf360ba764028374cdd6991a
Shadman Shakib <sm.shakib007@gmail.com>
2021-10-27 21:54:50 +0600

    TaskTopbar

--- src/components/Mytask/MyTaskTopbar.tsx
+++ src/components/Mytask/MyTaskTopbar.tsx
@@ -13,25 +13,36 @@ function Topbar() {
       flexShrink={0}
       zIndex={200}
     >
-      <Box display="inline-flex">
-        <Box display="flex" minH="72px" maxW="100%" alignItems="center">
-          <h1>My&nbsp;Tasks</h1>
-          <Box
-            role="button"
-            aria-label="Show options"
-            aria-expanded="false"
-            tabIndex={0}
-          >
-            <svg
-              height="16px"
-              width="16px"
-              focusable="false"
-              viewBox="0 0 32 32"
+      <Box display="flex">
+        <Box display="flex" flexDir="column" minH="72px" maxW="100%">
+          <Box display="flex" alignItems="center">
+            <Box fontSize="20px" mr="6px" lineHeight="28px">
+              My&nbsp;Tasks
+            </Box>
+            <Box
+              role="button"
+              aria-label="Show options"
+              aria-expanded="false"
+              tabIndex={0}
             >
-              <path d="M16,22.5c-0.3,0-0.7-0.1-0.9-0.3l-11-9c-0.6-0.5-0.7-1.5-0.2-2.1s1.5-0.7,2.1-0.2l10,8.2l10-8.2c0.6-0.5,1.6-0.4,2.1,0.2c0.5,0.6,0.4,1.6-0.2,2.1l-11,9C16.7,22.4,16.3,22.5,16,22.5z"></path>
-            </svg>
+              <svg
+                height="16px"
+                width="16px"
+                focusable="false"
+                viewBox="0 0 32 32"
+              >
+                <path d="M16,22.5c-0.3,0-0.7-0.1-0.9-0.3l-11-9c-0.6-0.5-0.7-1.5-0.2-2.1s1.5-0.7,2.1-0.2l10,8.2l10-8.2c0.6-0.5,1.6-0.4,2.1,0.2c0.5,0.6,0.4,1.6-0.2,2.1l-11,9C16.7,22.4,16.3,22.5,16,22.5z"></path>
+              </svg>
+            </Box>
           </Box>
-          <Box></Box>
+          <Box>
+            <Box display="flex">
+              <Box display="">List</Box>
+              <Box ml="24px">Board</Box>
+              <Box ml="24px">Calendar</Box>
+              <Box ml="24px">Files</Box>
+            </Box>
+          </Box>
         </Box>
       </Box>
     </Box>
