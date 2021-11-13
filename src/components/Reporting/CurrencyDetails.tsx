71cf13787d76ffa20f82bbde92840288b5b6ab5e
Shadman Shakib <sm.shakib007@gmail.com>
2021-11-13 15:51:08 +0600

    Design Teaks

--- src/components/Reporting/CurrencyDetails.tsx
+++ src/components/Reporting/CurrencyDetails.tsx
@@ -11,14 +11,24 @@ function CurrencyDetails() {
     <Grid templateColumns="repeat(3, 1fr)" gap={5}>
       {data.map((item) => {
         return (
-          <Box display="flex" alignItems="center" bg="cyan.700" key={item.id}>
+          <Box
+            display="flex"
+            borderRadius="8"
+            boxShadow="0px 0px 10px rgba(0, 0, 0, 0.25)"
+            p={5}
+            alignItems="center"
+            bg="cyan.700"
+            key={item.id}
+          >
             <Box>
               <Image height="10" width="10" src={item.image} />
               <Text color="white"> {item.name}</Text>
             </Box>
+
             <Box>
               <Text color="white">Price: $ {item.current_price}</Text>
               <Text color="white">Change: {item.price_change_24h}</Text>
+              <Text>{item.price_change_percentage_24h}%</Text>
             </Box>
           </Box>
         );
