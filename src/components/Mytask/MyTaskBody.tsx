5e224e271162e0e9dfe2406a854be3806ff5a5e6
Shadman Shakib <sm.shakib007@gmail.com>
2021-11-20 09:07:24 +0600

    Mytask body

--- src/components/Mytask/MyTaskBody.tsx
+++ src/components/Mytask/MyTaskBody.tsx
@@ -1,34 +1,26 @@
 import React from "react";
 import { Box, Text } from "@chakra-ui/react";
-import { ListView } from "../Mytask";
+import { ListView, BodyHeader } from "../Mytask";
+import s from "./MyTask.module.css";
 function MyTaskBody() {
   return (
     <Box>
+      <BodyHeader />
       <Box
-        px="24px"
-        border="1px solid"
+        position="relative"
         display="flex"
-        alignItems="center"
-        h="56px"
-        zIndex={600}
-        bg="brand.bgWeak"
+        flex="1 1 auto"
+        flexDirection="column"
+        min-height="1px"
+        width="100%"
       >
-        <Box
-          display="flex"
-          borderTopRadius="6px"
-          borderBottomRadius="6px"
-          px="8px"
-          bg="brand.btnBgPrimary"
-          h="28px"
-          alignItems="center"
-        >
-          <svg fill="white" height="12" width="12" viewBox="0 0 24 24">
-            <path d="M10,10V4c0-1.1,0.9-2,2-2s2,0.9,2,2v6h6c1.1,0,2,0.9,2,2s-0.9,2-2,2h-6v6c0,1.1-0.9,2-2,2s-2-0.9-2-2v-6H4c-1.1,0-2-0.9-2-2s0.9-2,2-2H10z"></path>
-          </svg>
-          <Text color="white">Add Task</Text>
+        <Box className={s.SpreadsheetGridHeaderStructure}>
+          <Box className={s.SpreadsheetGridHeaderStructureleft}>
+            <Text color="brand.weak">Task Name</Text>
+          </Box>
         </Box>
+        <ListView />
       </Box>
-      <ListView />
     </Box>
   );
 }
